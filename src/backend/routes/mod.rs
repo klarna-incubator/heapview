ab9f382b80f021fbe607d876fc5574225e52669e
Piotr <piotr.golab@klarna.com>
2020-08-14 18:30:05 +0200

    Allow CORS

--- src/backend/routes/mod.rs
+++ src/backend/routes/mod.rs
@@ -1,7 +1,35 @@
+use std::fs;
+
 use gotham::router::builder::*;
 use gotham::router::Router;
+use gotham::hyper::{Body, Response, StatusCode};
+use gotham::state::State;
+use gotham::helpers::http::response::create_response;
+use mime;
+
 mod analysis;
 
+
+pub fn handler(state: State) -> (State, Response<Body>) {
+
+    // file
+    let data = fs::read_to_string("/tmp/stats.json").expect("NOPE");
+
+    let mut response = create_response(
+        &state,
+        StatusCode::OK,
+        mime::TEXT_PLAIN,
+        data,
+    );
+
+    {
+        let headers = response.headers_mut();
+        headers.insert("Access-Control-Allow-Origin", "*".parse().unwrap());
+        headers.insert("Content-Type", "application/json".parse().unwrap());
+    };
+
+    (state, response)
+}
 
 pub fn heapview_router() -> Router {
     // let heapdumpMiddleware = StateMiddleware::with(heapdump);
@@ -20,7 +48,7 @@ pub fn heapview_router() -> Router {
 
     build_simple_router(|route| {
         route.scope("/analysis", |route| {
-            route.get("/").to_file("/tmp/stats.json") //.to(analysis::index_handler)
+            route.get("/").to(handler)
         })
     })
 }
