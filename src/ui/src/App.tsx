cc61828bef16b2f047410a0e315b216b114fa97d
Federico Ratier <federico.ratier@klarna.com>
2020-08-14 14:42:45 +0200

    Parse of contract proposed

--- src/ui/src/App.tsx
+++ src/ui/src/App.tsx
@@ -6,13 +6,11 @@ import Chart from './components/Chart';
 const exampleContract = {
   totalBytes: 100,
   categories: [
-    ['a', 30],
-    ['b', 30],
+    ['a', 20],
+    ['b', 40],
     ['c', 40],
   ],
 };
-
-const d3exampleData = [30, 30];
 
 interface AppProps {}
 
@@ -28,7 +26,6 @@ const App = ({}: AppProps) => {
     requestFromServer();
   }, []);
 
-  console.log(data);
   return (
     <div className="App">
       <div className="App-container">
--- src/ui/src/components/Chart/Chart.tsx
+++ src/ui/src/components/Chart/Chart.tsx
@@ -1,20 +1,39 @@
 import React, { useEffect, createRef } from 'react';
 import Slice from './Slice';
 import * as d3 from 'd3'; // @to-do should change the package to just d3 modules we are actually using
+import { map, applySpec, head, last } from 'ramda';
 
+interface Data {
+  categories: Array<any>;
+  totalBytes: number;
+}
 interface ChartProps {
-  data: Array<number>;
+  data: Data;
 }
 
+const applyKeyValue = applySpec({
+  key: head,
+  value: last,
+});
+
+const transformEntries = map(applyKeyValue);
+
 const Chart = ({ data }: ChartProps) => {
+  const { categories } = data;
+
   const sunburstChartRef = createRef();
 
   /** @to-do Should this be configurable by user? */
   const width = 500;
   const height = 500;
 
+  const entries = transformEntries(categories);
+
   /** @to-do switch to exampleContract and add labels */
-  let pie = d3.pie()(data);
+  let pie = d3
+    .pie()
+    .sort(null)
+    .value((d) => d.value)(entries);
 
   return (
     <svg height={height} width={width} ref={sunburstChartRef}>
