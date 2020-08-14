1c8963478556fe7197b2c0941366bf9aa0f99940
Jonas Cwojdzinski <jonas.cwojdzinski@klarna.com>
2020-08-14 14:31:56 +0200

    add http client

--- src/ui/src/App.tsx
+++ src/ui/src/App.tsx
@@ -1,28 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import axios from 'axios';
 
 interface AppProps {}
 
-function App({}: AppProps) {
+const App = ({}: AppProps) => {
+  const [data, setData] = useState({});
+  useEffect(() => {
+    const requestFromServer = async () => {
+      const res = await axios.request({
+        url: 'https://reqres.in/api/unknown',
+      });
+      setData(res.data);
+    };
+    requestFromServer();
+  }, []);
+
+  console.log(data);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-container">Here</div>
     </div>
   );
-}
+};
 
 export default App;
